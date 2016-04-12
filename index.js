5e1c50eb66c8f7b54abf97b1c06db5ee7d6fc199
molee1905 <mr.lee1905@gmail.com>
2016-04-12 18:23:41 +0800

    eslintv2.7 rules

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 module.exports = {
-    extends: [
+    'extends': [
         'eslint-config-sm/lib/env',
         'eslint-config-sm/lib/globals',
         'eslint-config-sm/lib/strict',
         'eslint-config-sm/lib/variables',
         'eslint-config-sm/lib/best',
         'eslint-config-sm/lib/stylistic'
-        
     ]
 };
